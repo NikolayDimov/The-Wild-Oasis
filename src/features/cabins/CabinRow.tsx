bcaa7420f6c8e0bfc181c9c7127b2eff08ccd6ec
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-08 17:57:56 +0300

    feat(Cabin): useQuery

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -1,28 +1,18 @@
 import styled from "styled-components";
-import { HiPencil, HiTrash, HiSquare2Stack } from "react-icons/hi2";
-
-import Menus from "../../ui/Menus";
-import Modal from "../../ui/Modal";
-import ConfirmDelete from "../../ui/ConfirmDelete";
-import Table from "../../ui/Table";
-
-import { formatCurrency } from "utils/helpers";
-import { useDeleteCabin } from "./useDeleteCabin";
-import { useCreateCabin } from "./useCreateCabin";
-import CreateCabinForm from "./CreateCabinForm";
+import { formatCurrency } from "../../utils/helpers";
 
 // v1
-// const TableRow = styled.div`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-//   padding: 1.4rem 2.4rem;
+const TableRow = styled.div`
+    display: grid;
+    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+    column-gap: 2.4rem;
+    align-items: center;
+    padding: 1.4rem 2.4rem;
 
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-// `;
+    &:not(:last-child) {
+        border-bottom: 1px solid var(--color-grey-100);
+    }
+`;
 
 const Img = styled.img`
     display: block;
@@ -55,22 +45,8 @@ const Discount = styled.div`
 function CabinRow({ cabin }) {
     const { id: cabinId, name, maxCapacity, regularPrice, discount, image, description } = cabin;
 
-    const { mutate: deleteCabin, isLoading: isDeleting } = useDeleteCabin();
-    const { mutate: createCabin } = useCreateCabin();
-
-    function handleDuplicate() {
-        createCabin({
-            name: `${name} duplicate`,
-            maxCapacity,
-            regularPrice,
-            discount,
-            image,
-            description,
-        });
-    }
-
     return (
-        <Table.Row role="row">
+        <TableRow role="row">
             <Img src={image} alt={`Cabin ${name}`} />
 
             <Cabin>{name}</Cabin>
@@ -81,7 +57,7 @@ function CabinRow({ cabin }) {
 
             {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
 
-            <Modal>
+            {/* <Modal>
                 <Menus.Menu>
                     <Menus.Toggle id={cabinId} />
 
@@ -94,14 +70,14 @@ function CabinRow({ cabin }) {
                             <Menus.Button icon={<HiPencil />}>Edit cabin</Menus.Button>
                         </Modal.Toggle>
 
-                        {/* Now it gets a bit confusing... */}
+                       
                         <Modal.Toggle opens="delete">
                             <Menus.Button icon={<HiTrash />}>Delete cabin</Menus.Button>
                         </Modal.Toggle>
                     </Menus.List>
                 </Menus.Menu>
 
-                {/* This needs to be OUTSIDE of the menu, which in no problem. The compound component gives us this flexibility */}
+            
                 <Modal.Window name="edit">
                     <CreateCabinForm cabinToEdit={cabin} />
                 </Modal.Window>
@@ -109,7 +85,7 @@ function CabinRow({ cabin }) {
                 <Modal.Window name="delete">
                     <ConfirmDelete resource="cabin" onConfirm={() => deleteCabin(cabinId)} disabled={isDeleting} />
                 </Modal.Window>
-            </Modal>
+            </Modal> */}
 
             {/* <div>
         <ButtonWithConfirm
@@ -121,10 +97,12 @@ function CabinRow({ cabin }) {
         >
           Delete
         </ButtonWithConfirm>
+        
 
         <Link to={`/cabins/${cabinId}`}>Details &rarr;</Link>
       </div> */}
-        </Table.Row>
+            <button>Delete</button>
+        </TableRow>
     );
 }
 
