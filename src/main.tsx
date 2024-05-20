5f6633e332200c601eb6b63834c80fa9652bc927
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-20 23:41:15 +0300

    Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => window.location.replace("/")}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorBoundary>
     </React.StrictMode>
 );
